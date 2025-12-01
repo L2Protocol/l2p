749d5e16687736d6752ba11370805b8fb4d42730
L2Protocol <github@l2protocol.com>
2025-12-01 11:51:24 +0000

    genesis: Update genesis hash
    Updated the genesisg hash

--- params/config.go
+++ params/config.go
@@ -33,7 +33,7 @@ var (
 	BSCGenesisHash    = common.HexToHash("0x0d21840abff46b96c84b2ac9e10e4f5cdaeb5693cb665db62a2f3b02d2d57b5b")
 	ChapelGenesisHash = common.HexToHash("0x6d3c66c5357ec91d5c43af47e234a939b22557cbb552dc45bebbceeed90fbe34")
 	RialtoGenesisHash = common.HexToHash("0xee835a629f9cf5510b48b6ba41d69e0ff7d6ef10f977166ef939db41f59f5501")
-	L2PGenesisHash    = common.HexToHash("0xc46549d42dff9739a26942723da060ab4e8753060413b32bc8942e9ef84d3cce")
+	L2PGenesisHash    = common.HexToHash("0xf75a882aeb000f5b65abc32f56967fa898e6a67e05a16d079796e0f6ee774bbe")
 )
 
 func newUint64(val uint64) *uint64 { return &val }
