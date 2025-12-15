6e6730841850f3d3c5070a9162d1e713a0562ebd
L2Protocol <github@l2protocol.com>
2025-12-15 12:26:18 +0000

    test: fix unit tests
    Fix failing tests

--- core/systemcontracts/upgrade_test.go
+++ core/systemcontracts/upgrade_test.go
@@ -30,7 +30,7 @@ func TestAllCodesHash(t *testing.T) {
 
 	allCodes := make([]byte, 0, 10_000_000)
 	for _, hardfork := range upgradesList {
-		for _, network := range []string{mainNet, chapelNet} {
+		for _, network := range []string{mainNet} {
 			allCodes = append(allCodes, []byte(network)...)
 			if hardfork[network] != nil {
 				for _, addressConfig := range hardfork[network].Configs {
@@ -41,7 +41,7 @@ func TestAllCodesHash(t *testing.T) {
 		}
 	}
 	allCodeHash := sha256.Sum256(allCodes)
-	require.Equal(t, allCodeHash[:], common.Hex2Bytes("833cc0fc87c46ad8a223e44ccfdc16a51a7e7383525136441bd0c730f06023df"))
+	require.Equal(t, common.Hex2Bytes("46e37efb0303f87fc998b8c26ad90a3ec7b7a29cbf4d9ceba188151a2bdb836b"), allCodeHash[:])
 }
 
 func TestUpgradeBuildInSystemContractNilInterface(t *testing.T) {
