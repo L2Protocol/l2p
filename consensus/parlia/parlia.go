7a8259bda8988f2a993c043eee37e4d5ed00598b
L2Protocol <github@l2protocol.com>
2026-09-12 14:34:13 +0200

    Remove references to contracts that we do not use

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -102,20 +102,14 @@ var (
 	attestationVoteCountGauge         = metrics.NewRegisteredGauge("parlia/attestation/voteCount", nil)
 
 	systemContracts = map[common.Address]bool{
-		common.HexToAddress(systemcontracts.ValidatorContract):          true,
-		common.HexToAddress(systemcontracts.SlashContract):              true,
-		common.HexToAddress(systemcontracts.SystemRewardContract):       true,
-		common.HexToAddress(systemcontracts.LightClientContract):        true,
-		common.HexToAddress(systemcontracts.RelayerHubContract):         true,
-		common.HexToAddress(systemcontracts.GovHubContract):             true,
-		common.HexToAddress(systemcontracts.TokenHubContract):           true,
-		common.HexToAddress(systemcontracts.RelayerIncentivizeContract): true,
-		common.HexToAddress(systemcontracts.CrossChainContract):         true,
-		common.HexToAddress(systemcontracts.StakeHubContract):           true,
-		common.HexToAddress(systemcontracts.GovernorContract):           true,
-		common.HexToAddress(systemcontracts.GovTokenContract):           true,
-		common.HexToAddress(systemcontracts.TimelockContract):           true,
-		common.HexToAddress(systemcontracts.TokenRecoverPortalContract): true,
+		common.HexToAddress(systemcontracts.ValidatorContract):    true,
+		common.HexToAddress(systemcontracts.SlashContract):        true,
+		common.HexToAddress(systemcontracts.SystemRewardContract): true,
+		common.HexToAddress(systemcontracts.GovHubContract):       true,
+		common.HexToAddress(systemcontracts.StakeHubContract):     true,
+		common.HexToAddress(systemcontracts.GovernorContract):     true,
+		common.HexToAddress(systemcontracts.GovTokenContract):     true,
+		common.HexToAddress(systemcontracts.TimelockContract):     true,
 	}
 )
 
